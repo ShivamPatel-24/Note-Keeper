6a2cae674a9880d8baa26e5b61adb4ed3d37f01b
Shivam Patel <shivampatel@Shivams-Air.attlocal.net>
2022-03-25 00:03:16 -0400

    add notes dynamically

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import reactDom from "react-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Note from "./components/Note.jsx";
+import notes from "./notes";
+
+function addNotes(note){
+    return (
+        <Note 
+            key={note.id}
+            title={note.title}
+            content={note.content}
+        />
+    );
+}
 
 reactDom.render(
     <div>
         <Header />
+        {notes.map(addNotes)}
         <Footer />
-        <Note />
     </div>
     ,document.getElementById("root")
 )
